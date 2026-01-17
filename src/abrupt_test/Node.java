51dfcac1d48ea9158614bb4f81da0a80699173d8
Jindra Petřík <jindra.petrik@gmail.com>
2026-01-17 12:46:25 +0100

    Init

---
+++ src/abrupt_test/Node.java
@@ -0,0 +1,9 @@
+package abrupt_test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    public List<Node> preds = new ArrayList<>();
+    public List<Node> succs = new ArrayList<>();
+}
